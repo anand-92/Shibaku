ae12c3478d7c29305dfee2fb9faa455ceec7e314
Nik Anand <nikhil.anand@dcsg.com>
2023-04-21 15:40:11 -0400

    Finally fixed search bug

--- screens/MainApp.tsx
+++ screens/MainApp.tsx
@@ -1,54 +1,54 @@
 import { View } from '../components/Themed';
 import PlayerInfoScreen from "../components/PlayerInfo";
 import {styles} from "../resources/stylesheets";
-import {Image, TextInput} from "react-native";
+import {Image, Text, TextInput} from "react-native";
 import React, {useState} from "react";
 import SearchPopUp from "../components/SearchPopUp";
 import {stockPic} from "../resources/pictures";
 
 export default function MainApp() {
-    const [searchInfo, setSearchInfo] = useState<any>([]);
     const [searchVisible, setSearchVisible] = useState(false);
     const [statInfo, setStatInfo] = useState<any>("Loading...");
     const [playerPic, setPlayerPic] = useState<any>(stockPic);
+    const [text, onChangeText] = React.useState('Search A Player...');
+    let searchInfo: any;
 
-    function getInfo(player: string) {
-        fetchSearch(player).then( async () => {
-            if (searchInfo.data != undefined) {
-                console.log(JSON.stringify(searchInfo.data[0].first_name))
-                if (searchInfo.data[0].first_name == "No Player Found") {
-                    setStatInfo("No Player Found");
-                } else {
-                    setStatInfo(`Name: ${searchInfo.data[0].first_name} ${searchInfo.data[0].last_name}\n` +
-                        `Height: ${searchInfo.data[0].height_feet}\'${searchInfo.data[0].height_inches}\"\n` +
-                        `Position: ${searchInfo.data[0].position}\n` +
-                        `PPG: ${searchInfo.data[0].pts}`);
-                    setPlayerPic("https://www.basketball-reference.com/req/202106291/images/players/"
-                        +(searchInfo.data[0].last_name.substring(0,5)+searchInfo.data[0].first_name.substring(0,2)).toLowerCase()+"01.jpg");
-                    console.log(playerPic);
-                }
-            }
-        })
+    async function getJSON(name: string) {
+        return await fetch("http://localhost:8081/player/"+name+"/seasonStats")
+            .then((response)=>response.json())
+            .then((responseJson)=>{return responseJson});
     }
 
     async function fetchSearch(name: string) {
-        const searchStatsResponse = await fetch(
-            "http://localhost:8081/player/"+name+"/seasonStats"
-        );
-        searchStatsResponse
-            .json()
-            .then((searchStatsResponse) => setSearchInfo(searchStatsResponse));
+        const searchStatsResponse = await getJSON(name);
+        console.log(searchStatsResponse);
+        searchInfo = searchStatsResponse;
+        console.log(JSON.stringify(searchInfo));
     }
 
-     async function onSearch(text: string) {
-         getInfo(text);
+     async function onSearch(player: string) {
          setSearchVisible(true);
+         await fetchSearch(player).then( () => {
+             if (searchInfo.data != undefined) {
+                 if (searchInfo.data[0].first_name == "No Player Found") {
+                     setStatInfo("No Player Found");
+                 } else {
+                     setStatInfo(`Name: ${searchInfo.data[0].first_name} ${searchInfo.data[0].last_name}\n` +
+                         `Height: ${searchInfo.data[0].height_feet}\'${searchInfo.data[0].height_inches}\"\n` +
+                         `Position: ${searchInfo.data[0].position}\n` +
+                         `PPG: ${searchInfo.data[0].pts}`);
+                     setPlayerPic("https://www.basketball-reference.com/req/202106291/images/players/"
+                         +(searchInfo.data[0].last_name.substring(0,5)+searchInfo.data[0].first_name.substring(0,2)).toLowerCase()+"01.jpg");
+                 }
+             }
+         })
      }
-    const [text, onChangeText] = React.useState('Search A Player...');
 
-  return (
+    return (
     <View style={styles.container}>
         <Image></Image>
+        <Text style={styles.instructions}>Enter a player's first & last Name to search.<br/>Iconic players like "LeBron"
+            can be found with their unique name.</Text>
         <TextInput
             style={styles.input}
             onChangeText={onChangeText}
