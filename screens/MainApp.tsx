627d3e633ca3b219c2156f5a1c8d033ae4b7cd6e
Nik <anand.92@osu.edu>
2023-07-15 02:40:21 -0400

    fix styling

--- screens/MainApp.tsx
+++ screens/MainApp.tsx
@@ -8,13 +8,13 @@ export default function MainApp() {
 
   return (
     <View style={styles.container}>
-      <Text style={{ paddingBottom: 20, fontWeight: "bold", color: "white", fontSize: 14 }}>
+      <Text style={{ paddingBottom: 20, fontWeight: "bold", color: "white", fontSize: 14, textAlign: 'center' }}>
         Note: Technical Analysis of Small-Cap Cryptocurrencies Is Unreliable
       </Text>
       <iframe
         id="dextools-widget"
         title="DEXTools Trading Chart"
-        width="100%"
+        width="80%"
         height="100%"
         src="https://www.dextools.io/widget-chart/en/ether/pe-light/0x04f01db076c85ea9a27c84c83e13b
               166fe9db95c?theme=dark&chartType=2&chartResolution=30&drawingToolbars=false"
