10caaa7ce94ba854b272c6381c9c139e2b136e53
Nik <anand.92@osu.edu>
2023-07-16 00:21:28 -0400

    fix styling

--- screens/MainApp.tsx
+++ screens/MainApp.tsx
@@ -16,7 +16,7 @@ export default function MainApp() {
         title="DEXTools Trading Chart"
         width="90%"
         height="100%"
-        src="https://www.dextools.io/widget-chart/en/ether/pe-light0x04Ec425e0773A06e504b81924ae349D758edEbd1?theme=dark&chartType=2&chartResolution=30&drawingToolbars=false"
+        src="https://www.dextools.io/widget-chart/en/ether/pe-light/0x04Ec425e0773A06e504b81924ae349D758edEbd1?theme=dark&chartType=2&chartResolution=30&drawingToolbars=false"
       ></iframe>
     </View>
   );
